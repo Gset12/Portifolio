ab00c65fd22d571b1879c4b0245ba9b5da6fb807
Gset12 <99854346+Gset12@users.noreply.github.com>
2023-03-24 22:45:30 +0000

    Jogo terminado

--- Jogo da Velha/scripts.js
+++ Jogo da Velha/scripts.js
@@ -1,13 +1,3 @@
-var posicaoGanhar = [
-    [1, 2, 3],
-    [1, 5, 7],
-    [3, 6, 9],
-    [7, 8, 9],
-    [4, 5, 6],
-    [1, 5, 9],
-    [3, 5, 7]
-] 
- 
 var posAtual= "X";
 
 function blocoAcionado(value){
@@ -16,5 +6,5 @@ function blocoAcionado(value){
         document.getElementById(casa).innerHTML = posAtual;
         posAtual = (posAtual === "X") ? "O":"X";
     }
-
 }
+
